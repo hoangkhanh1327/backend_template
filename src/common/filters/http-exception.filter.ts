845e61d3b1ccf9b5b6b82c84883f1a55b95c6e3a
khanhth <khanh.tranhoang1999@gmail.com>
2026-08-10 08:51:28 +0700

    init repo

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { Catch, ExceptionFilter, ArgumentsHost, HttpException } from "@nestjs/common";
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse();
+        const status =
+            exception instanceof HttpException ? exception.getStatus() : 500;
+        const message =
+            exception instanceof HttpException
+                ? exception.getResponse()
+                : "Internal server error";
+
+        response.status(status).json({
+            statusCode: status,
+            message,
+            timestamp: new Date().toISOString(),
+        });
+    }
+}
